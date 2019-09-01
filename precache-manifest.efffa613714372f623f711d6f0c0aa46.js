85a7098b54ecfd56aaa9ed29f0c379574cd0588e
tarasprocishin <taras1procishin@gmail.com>
2019-09-01 19:53:37 +0300

    Updates

---
+++ precache-manifest.efffa613714372f623f711d6f0c0aa46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c46abab5129f6f4b89169836e19c0cc",
+    "url": "/to_do_app/index.html"
+  },
+  {
+    "revision": "b2f2cb1b9aa0fe87ea83",
+    "url": "/to_do_app/static/css/main.04fc4891.chunk.css"
+  },
+  {
+    "revision": "88d40ee520bd14678ed1",
+    "url": "/to_do_app/static/js/2.dc12531a.chunk.js"
+  },
+  {
+    "revision": "b2f2cb1b9aa0fe87ea83",
+    "url": "/to_do_app/static/js/main.97c2827a.chunk.js"
+  },
+  {
+    "revision": "6fae0c45b351f4d664b3",
+    "url": "/to_do_app/static/js/runtime~main.f4485b18.js"
+  },
+  {
+    "revision": "c530de65491833abcad1092ab8afd831",
+    "url": "/to_do_app/static/media/checked.c530de65.svg"
+  },
+  {
+    "revision": "2a7db503f7dc7c812f08e1e56cd84ac9",
+    "url": "/to_do_app/static/media/circle.2a7db503.svg"
+  }
+]);
